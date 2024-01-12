89ecb35fe7982b5fc6b3325cc44be4849186348d
dianamihaica <142227552+dianamihaica@users.noreply.github.com>
2024-01-12 16:42:40 +0200

    js06

--- 01.js
+++
@@ -1,9 +0,0 @@
-var categoriesList = document.getElementById('categories');
-var categories = categoriesList.querySelectorAll('li.item');
-console.log('Number of categories:', categories.length);
-categories.forEach(function(category) {
-  var categoryName = category.querySelector('h2').textContent;
-  var categoryElements = category.querySelectorAll('ul li');
-  console.log('\nCategory:', categoryName);
-  console.log('Elements:', categoryElements.length);
-});
